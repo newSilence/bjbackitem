f27f024d269e8b83037cb3ee3664dbdcca0922ed
shui <x9527shui@163.com>
2019-12-02 08:49:38 +0800

    change some

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import  VueQuillEditor from 'vue-quill-editor'
+// require styles 引入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 import App from './App'
 import router from './router'
 // import {http} from './request/http'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.use(VueQuillEditor)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
